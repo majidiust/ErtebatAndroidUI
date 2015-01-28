47682ee5ce45011a37946b16f760d6abefe6d760
Morteza Sabetraftar <morteza.sabetraftar@gmail.com>
2015-01-28 12:00:23 +0330

    + Added code to save pictures received in chat + Fixed a small mistake in a layout

--- src/org/ertebat/ui/ChatMessageAdapter.java
+++ src/org/ertebat/ui/ChatMessageAdapter.java
@@ -3,6 +3,9 @@ package org.ertebat.ui;
 import android.content.Context;
 import android.content.res.Resources.NotFoundException;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +15,15 @@ import android.widget.TextView;
 
 import com.hipmob.gifanimationdrawable.GifAnimationDrawable;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Picasso.LoadedFrom;
+import com.squareup.picasso.Target;
 
 import org.ertebat.R;
 import org.ertebat.schema.SettingSchema;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -27,6 +35,8 @@ public class ChatMessageAdapter extends ArrayAdapter<ChatMessage> {
     private Bitmap mContactPicture;
     private List<ChatMessage> mDataSet;
 
+    private Target mPictureTarget;
+
     public ChatMessageAdapter(Context context, Bitmap contactPic, List<ChatMessage> objects) {
         super(context, R.id.txtChatMessageListItemContent, objects);
 
@@ -37,7 +47,7 @@ public class ChatMessageAdapter extends ArrayAdapter<ChatMessage> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ChatMessage message = mDataSet.get(position);
+        final ChatMessage message = mDataSet.get(position);
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View itemView = null;
         TextView text;
@@ -55,7 +65,40 @@ public class ChatMessageAdapter extends ArrayAdapter<ChatMessage> {
             GifAnimationDrawable dr = null;
             itemView = inflater.inflate(message.IsSenderSelf ? R.layout.data_list_item_chat_message_picture_right
                     : R.layout.data_list_item_chat_message_picture_left, null, true);
-            ImageView image = (ImageView) itemView.findViewById(R.id.imgChatMessageListItemContent);
+            final ImageView image = (ImageView) itemView.findViewById(R.id.imgChatMessageListItemContent);
+
+            mPictureTarget = new Target() {
+
+                @Override
+                public void onPrepareLoad(Drawable placeHolderDrawable) {
+                    image.setImageDrawable(placeHolderDrawable);
+                }
+
+                @Override
+                public void onBitmapLoaded(Bitmap bitmap, LoadedFrom from) {
+                    try {
+                        File pictureFileDirectory = new File(Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_PICTURES).getPath()
+                                + "/" + BaseActivity.PICTURES_DIRECTORY_NAME);
+                        pictureFileDirectory.mkdirs();
+                        File pictureFile = new File(pictureFileDirectory, message.MessageId);
+                        FileOutputStream output = new FileOutputStream(pictureFile);
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, output);
+
+                        image.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException ex) {
+                        if (ex.getMessage() != null) {
+                            Log.d(TAG, ex.getMessage());
+                        }
+                    }
+                }
+
+                @Override
+                public void onBitmapFailed(Drawable errorDrawable) {
+                    image.setImageDrawable(errorDrawable);
+                }
+            };
+
             try {
                 dr = (new GifAnimationDrawable(getContext().getResources().openRawResource(R.drawable.ajax_loader)));
                 dr.setVisible(true, true);
@@ -87,11 +130,9 @@ public class ChatMessageAdapter extends ArrayAdapter<ChatMessage> {
                         .resize(width, width).centerInside()
                         .placeholder(org.ertebat.R.drawable.ic_default_user_picture)
                         .error(org.ertebat.R.drawable.ic_default_user_picture).into(image);
-
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-
         } else { // set the delivery icon
             imageDelivery = (ImageView) itemView.findViewById(R.id.imageChatMessageListItemDeliveryStatus);
             imageDelivery.setBackgroundResource(message.IsDelivered ? R.drawable.ic_message_delivered
