bbe93728f67376a21780a04fdef3d83753151f89
Morteza Sabetraftar <morteza.sabetraftar@gmail.com>
2015-02-07 01:33:55 +0330

    +Made some changes in chat message list items

--- src/org/ertebat/ui/MainActivity.java
+++ src/org/ertebat/ui/MainActivity.java
@@ -1,34 +1,12 @@
 package org.ertebat.ui;
-import org.doubango.ngn.NgnEngine;
-import org.doubango.ngn.events.NgnEventArgs;
-import org.doubango.ngn.events.NgnInviteEventArgs;
-import org.doubango.ngn.events.NgnInviteEventTypes;
-import org.doubango.ngn.events.NgnMessagingEventArgs;
-import org.doubango.ngn.events.NgnMsrpEventArgs;
-import org.doubango.ngn.events.NgnRegistrationEventArgs;
-import org.doubango.ngn.media.NgnMediaType;
-import org.doubango.ngn.model.NgnHistorySMSEvent;
-import org.doubango.ngn.model.NgnHistoryEvent.StatusType;
-import org.doubango.ngn.sip.NgnAVSession;
-import org.doubango.ngn.sip.NgnMsrpSession;
-import org.doubango.ngn.utils.NgnDateTimeUtils;
-import org.doubango.ngn.utils.NgnStringUtils;
-import org.doubango.ngn.utils.NgnUriUtils;
-import org.ertebat.R;
-import org.ertebat.R.id;
-import org.ertebat.R.layout;
 
+import org.ertebat.R;
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.app.ActionBar.Tab;
 import android.app.ActionBar.TabListener;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.RemoteException;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -46,9 +24,10 @@ public class MainActivity extends BaseActivity {
 	private TabListener mTabListener;
 	private HomePagerAdapter mAdapter;
 	private ActionBar mActionBar;
-	private String[] tabNames = { "ê› êÊ Â«", "œÊ” «‰", " „«”" };
+	private final String[] tabNames = { "⁄ØŸÅÿ™⁄ØŸàŸáÿß", "ÿØŸàÿ≥ÿ™ÿßŸÜ", "ÿ™ŸÖÿßÿ≥" };
 
-	private static int mCount = 0 ;
+	private static int mCount = 0;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -58,7 +37,7 @@ public class MainActivity extends BaseActivity {
 
 		mActionBar = getActionBar();
 
-		mPager = (ViewPager)findViewById(R.id.pagerHome);
+		mPager = (ViewPager) findViewById(R.id.pagerHome);
 		mAdapter = new HomePagerAdapter(getSupportFragmentManager(), this);
 		mPager.setAdapter(mAdapter);
 		mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -101,7 +80,7 @@ public class MainActivity extends BaseActivity {
 					mPager.setCurrentItem(tab.getPosition());
 				} catch (Exception ex) {
 					Log.d(TAG, ex.getMessage());
-				}				
+				}
 			}
 
 			@Override
@@ -115,15 +94,13 @@ public class MainActivity extends BaseActivity {
 			LayoutInflater inflator = LayoutInflater.from(this);
 			View view = inflator.inflate(R.layout.tab_title, null);
 
-			TextView text = (TextView)view.findViewById(R.id.txtTabTitle);
+			TextView text = (TextView) view.findViewById(R.id.txtTabTitle);
 			text.setText(tabName);
 			text.setTypeface(FontKoodak);
 
-			mActionBar.addTab(mActionBar.newTab()
-					.setCustomView(view)
-					.setTabListener(mTabListener));
+			mActionBar.addTab(mActionBar.newTab().setCustomView(view).setTabListener(mTabListener));
 		}
-		if(mCount == 0){
+		if (mCount == 0) {
 			mCount++;
 			new Thread(new Runnable() {
 				@Override
@@ -132,7 +109,7 @@ public class MainActivity extends BaseActivity {
 						Thread.sleep(1000);
 						mWebsocketService.getMyProfile();
 						getFriendList();
-						//mWebsocketService.getGroupRooms();
+						// mWebsocketService.getGroupRooms();
 						showToast("getFriendList : " + mCount);
 					} catch (Exception e) {
 						Log.d(TAG, e.getMessage());
@@ -144,7 +121,8 @@ public class MainActivity extends BaseActivity {
 	}
 
 	public static class HomePagerAdapter extends FragmentPagerAdapter {
-		private MainActivity mParent;
+		private final MainActivity mParent;
+
 		public HomePagerAdapter(FragmentManager fm, MainActivity parent) {
 			super(fm);
 			mParent = parent;
@@ -163,7 +141,8 @@ public class MainActivity extends BaseActivity {
 			case 1:
 				return new ContactListFragment();
 			case 2:
-				//				Inbox.MessageList = new ArrayList<Message>(mParent.loadMessages());
+				// Inbox.MessageList = new
+				// ArrayList<Message>(mParent.loadMessages());
 				return new CallFragment();
 			default:
 				return null;
@@ -177,7 +156,7 @@ public class MainActivity extends BaseActivity {
 
 		if (IsTablet)
 			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
-		
+
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
